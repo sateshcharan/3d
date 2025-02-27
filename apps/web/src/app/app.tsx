fe2c1ee0fcacd0bfd5c4148fed9a87f00155a656
sateshcharan <sateshcharans@gmail.com>
2025-02-27 09:49:55 +0000

    initial commit

--- apps/web/src/app/app.tsx
+++ apps/web/src/app/app.tsx
@@ -1,9 +1,25 @@
-import NxWelcome from './nx-welcome';
+import { useState, useEffect } from 'react';
 
 export function App() {
+  const [data, setData] = useState('');
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        const joke = await fetch('http://localhost:3333/api');
+        const data = await joke.json();
+        setData(data.message);
+      } catch (error) {
+        console.error(error);
+      }
+    }
+    fetchData();
+  }, []);
+
   return (
-    <div>
-      <NxWelcome title="web" />
+    <div className="w-full h-screen flex justify-center items-center">
+      <div className="p-4 bg-slate-100 rounded-2xl shadow-xl">
+        <h2 className="font-semibold capitalize">{data}</h2>
+      </div>
     </div>
   );
 }
